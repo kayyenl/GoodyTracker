eac62e1269196b0b96646883329d2f9ec8fff9e0
kayyenl <yenyenlee9@gmail.com>
2023-05-25 02:55:42 +0800

    added protect functionality to prevent unauthorized user requests

--- backend/middleWare/authMiddleware.js
+++ backend/middleWare/authMiddleware.js
@@ -1,3 +1,33 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
+
+const protect = asyncHandler(async (req, res, next) => {
+    try {        
+        const token = req.cookies.token
+        if (!token) {
+            res.status(401)  //this status code is for unauthorized response
+            throw new Error("Not authorized, please sign in again.")
+        }
+
+        //Verify Token
+        //we can use the jwt package to verify the authenticity of the token.
+        //resume: used jwt to verify tokens to protect unauthorized access efficiently
+        const verified = jwt.verify(token, process.env.JWT_SECRET)
+        //we can actually get the user id from token, cus we initialized a token using the users permanent id.
+        const user = await User.findById(verified.id).select("-password") //this syntax is useful to remove fields udw  to see. Eg. since we do not wish to return the user's password, we use the syntax to prevent sending it.
+
+        if (!user) {
+            res.status(401)
+            throw new Error("User not found.")
+        }
+
+        req.user = user
+        next()
+    } catch(error) {
+        res.status(401)  //this status code is for unauthorized response
+        throw new Error("Not authorized, please sign in again.")
+    }
+})
+
+module.exports = protect
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router() // a mini standalone app. like a small app placeholder.
 const { registerUser, loginUser, logout, getUser } = require('../controllers/userController')
+const protect = require('../middleWare/authMiddleware')
 
 router.post("/register", registerUser)
 //registeruser will be in another class, wrt to separation of concerns
@@ -9,7 +10,7 @@ router.get("/logout", logout)
 //logout will be a get request, because we arent sending any data
 //Both GET and POST method is used to transfer data from client to server in HTTP protocol but Main difference between POST and GET method is that GET carries request parameter appended in URL string while POST carries request parameter in message body which makes it more secure way of transferring data from client to server. 
 
-router.get('/getuser', getUser)
+router.get('/getuser', protect, getUser)
 //we need to protect this endpoint, by checking if the user is logged in, 
 //banning access to the endpoint of the user is not logged in.
 //we do this by a middleware function, that will be in the middleware folder cus it will be used in 
